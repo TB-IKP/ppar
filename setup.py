4f3bb725a363f473fe5d5a24d29dfac22026d32d
TB-IKP <beck@frib.msu.edu>
2025-10-31 20:33:40 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
         name='ppar',
-        version='0.0.1',
+        version='1.0.0',
         description='Analysis of parallel momentum distributions at NSCL/FRIB and RIBF',
         author='Tobias Beck',
-        author_email='beck@frib.msu.edu',
+        author_email='tobias.beck@kuleuven.be',
         license_files=('LICENSE'),
         python_requires='>=3.9',
         packages=['ppar'],
